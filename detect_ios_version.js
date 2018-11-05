e48dad123f05ef577976a6be42cfc641059ba929
Mike <goodayue@gmail.com>
2018-11-05 11:24:13 +0800

    [First Commit] upload file
    Will update code soon

---
+++ detect_ios_version.js
@@ -0,0 +1,7 @@
+function iOSversion() {
+    if (/iP(hone|od|ad)/.test(navigator.platform)) {
+        // supports iOS 2.0 and later: <https://bit.ly/TJjs1V>
+        var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+        return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+    }
+}
